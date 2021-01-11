f3d627c6f807b42981f3800dcf6438a35d04e7c1
dbdomino <dbdomino@naver.com>
2021-01-11 23:47:22 +0900

    this is first make project

---
+++ src/main/promo/helloWorld.java
@@ -0,0 +1,11 @@
+package main.promo;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello World !!");
+		// first make 2021.01.11
+	}
+
+}
